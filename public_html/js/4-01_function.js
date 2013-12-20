a016029c892e256a33576624e45d357d09212572
Eiichi Tanaka <Eiichi Tanaka@a1-win-fx8120>
2013-12-20 09:18:54 +0900

    P135の関数リテラル表現で定義するサンプルを作成

---
+++ public_html/js/4-01_function.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*******************************************************************************
+ * Fuction命令での関数定義のサンプル 
+ ******************************************************************************/
+function triangle(base, height) {
+    return base * height / 2;
+}
+
+document.writeln('三角形の面積：' + triangle(5, 2));
---
+++ public_html/js/4-03_functionLiteral.js
@@ -0,0 +1,13 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/*******************************************************************************
+ * 関数リテラル表現のサンプル
+ ******************************************************************************/
+var triangle = function(base, height) {
+    return base * height / 2;
+}
+
+document.writeln('三角形の面積：' + triangle(10, 2));
